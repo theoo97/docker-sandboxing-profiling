93ed9c1e80530b3c49c53d83f6d6fdb79969f3b2
Theo Chihaia <theo97chihaia@gmail.com>
2020-05-24 10:40:38 +0000

    simple script to run tests

--- run.sh
+++ run.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-cd $1
-sudo docker run -ti docker-sandboxing-profiling:$1-workload $2
+
+usage="Usage: $0 sandbox stressor load"
+usage_example="Example: $0 gvisor cpu 1"
+
+if [ "$#" -ne 3 ]; then
+	echo $usage
+	echo $usage_example	
+fi
+
+cd $2
+
+if [ "$1" == "no-sandbox" ]; then
+	./$2-workload.sh $3
+elif [ "$1" == "docker" ]; then
+	sudo docker run -ti docker-sandboxing-profiling:$2-workload $3
+elif [ "$1" == "gvisor" ]; then
+	sudo docker run --runtime=runc -ti docker-sandboxing-profiling:$2-workload $3
+fi
+
 cd ..
