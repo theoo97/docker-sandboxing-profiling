49a3e82d6879231770965ceae9f1ab4a63ab44d6
theo <theo97chihaia@gmail.com>
2020-05-24 10:44:05 +0300

    remove echo

--- cpu/cpu-workload.sh
+++ cpu/cpu-workload.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 ops=$(echo "$1*1358" | bc)
-echo $ops
 stress-ng --cpu 1 --cpu-ops $ops
--- file-io/file-io-workload.sh
+++ file-io/file-io-workload.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 ops=$(echo "$1*2500" | bc)
-echo $ops
 sudo stress-ng --iomix 1 --iomix-ops $ops
