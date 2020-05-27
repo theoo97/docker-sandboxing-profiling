2b1374049a9b7bda220ceaef21ae7407ca7182e9
Theo Chihaia <theo97chihaia@gmail.com>
2020-05-27 03:58:40 +0000

    Automated profiling

--- automated.sh
+++ automated.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-base_ops=(5 10 15 20 25 30 35 40 45 50)
+base_ops=($(seq 10 5 100))
 len=${#base_ops[@]}
 
-for (( i=0; i<$len; i++ ));
+function run {
+	WORKLOAD=$1
+	SANDBOX=$2
+
+	for (( i=0; i<$len; i++ ));
+	do
+		start=`date +%s%N`
+		./run.sh $SANDBOX $WORKLOAD ${base_ops[$i]} > /dev/null
+		end=`date +%s%N`
+
+		runtime=$(echo "scale=2;($end-$start)/1000000000" | bc)
+
+		runtimes[$i]=$runtime
+	done
+
+	echo "====>>>>> $WORKLOAD on $SANDBOX <<<<<===="
+	echo "base_ops=[${base_ops[@]}]" | sed 's/ /, /g'
+	echo "runtimes=[${runtimes[@]}]" | sed 's/ /, /g'
+	printf "\n\n\n"
+}  
+
+WORKLOADS="cpu memory-management process-management network file-io"
+SANDBOXES="no-sandbox docker gvisor kata"
+
+for WORKLOAD in $WORKLOADS
 do
-	start=`date +%s%N`
-	./run.sh no-sandbox process-management ${base_ops[$i]}
-	end=`date +%s%N`
-
-	runtime=$(echo "scale=2;($end-$start)/1000000000" | bc)
-	echo $runtime
-
-	runtimes[$i]=$runtime
+	for SANDBOX in $SANDBOXES
+	do
+		run $WORKLOAD $SANDBOX
+	done
 done
 
+echo $base_ops
 echo "runtimes=[${runtimes[@]}]" | sed 's/ /, /g'
 
 
